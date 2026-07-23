f337bd740c122f8b3d36ebdf718a37f8f73f12f0
oktei0226-lgtm <oktei0226@gmail.com>
2026-07-23 12:51:02 +0900

    python 연습코드 commit

---
+++ 1주차/day1/code.py
@@ -0,0 +1,8 @@
+
+a = int(input("숫자입력: "))
+
+b = int(input("두번째 숫자 입력: "))
+
+print(a+b)
+
+
---
+++ 1주차/day1/code3.py
@@ -0,0 +1,9 @@
+a=int(input(">>>"))
+
+if a % 2 == 0:
+    print("a는 짝수입니다")
+
+else :
+    print("a는 홀수입니다")
+
+
